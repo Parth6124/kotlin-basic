de6bacc9ff43affca9236a8d3c41427a3a53c623
Parth6124 <parmar6124@gmail.com>
2023-03-17 16:04:31 +0530

    Kotlin Basic
    This is kotlin basic

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main()
+{
+    println("My name is parth")
+    val aa="My name is parth"
+    val ab="PARTH"
+    val abd="parth"
+    val abc='P'
+    val a=20
+    val b=23
+    val c= a+b
+    println(c)
+    val d= 10.5
+    val f= 10.5
+    val h=d+f
+    val bb=10
+    val cc=6
+
+    println("My Name is ${bb+cc}")
+
+    println(h.toInt())
+    println(ab.toUpperCase())
+    println(abd.toLowerCase())
+    println(ab.capitalize())
+    println(abd.decapitalize())
+    println(ab[0])
+    println(ab.length)
+    println(ab.get(2))
+    println(aa.substring(2,7))
+}
